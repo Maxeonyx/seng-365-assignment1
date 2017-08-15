63428305f131ba7307c4456c36fae6747e846330
Maxwell Clarke <maxeonyx@gmail.com>
2017-08-15 21:36:25 +1200

    Create Projects Route (stub)

---
+++ routes/projects.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const validate = require('../validate.js');
+const db = require('../db.js');
+const auth = require('./auth.js');
+
+
+
+module.exports = router;
