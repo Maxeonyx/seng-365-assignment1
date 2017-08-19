574b7774e6cd7d071f56a6689c1d0acdb98cf6ee
Maxwell Clarke <maxeonyx@gmail.com>
2017-08-19 19:56:29 +1200

    use pastebin as a remote logging api :)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const db = require('./db.js');
 
@@ -15,7 +16,7 @@ app.use('/projects', require('./routes/projects.js'));
 
 db.initialise().then(() => {
 	
-	app.set('port', process.env.PORT || 8080);
+	app.set('port', process.env.PORT || 8081);
 
 	app.listen(app.get('port'), () => {
 		console.log("Listening on port: " + app.get('port'));
@@ -23,6 +24,16 @@ db.initialise().then(() => {
 
 })
 .catch((err) => {
+	request
+		.post('https://pastebin.com/api/api_post.php')
+		.form({
+			api_option: 'paste',
+			api_dev_key: 'cdd049ffe2fca48e12b062311dd95f1e',
+			api_user_key: '6a8d1227c643f3e341fd9c1725f07565',
+			api_paste_code: err.toString(),
+			api_paste_name: 'Express error',
+			api_paste_private: 2
+		});
 	console.log(err);
 	console.log("Failed to start server.")
 });
