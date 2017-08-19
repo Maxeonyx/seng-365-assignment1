760c37c3cd6d16705de2fddf6949274d68390136
Maxwell Clarke <maxeonyx@gmail.com>
2017-08-19 20:02:43 +1200

    pastebin 2

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use('/users', require('./routes/users.js'));
 app.use('/projects', require('./routes/projects.js'));
 
 db.initialise().then(() => {
-	
-	app.set('port', process.env.PORT || 8081);
+
+	app.set('port', process.env.PORT || 8080);
 
 	app.listen(app.get('port'), () => {
 		console.log("Listening on port: " + app.get('port'));
@@ -30,8 +30,8 @@ db.initialise().then(() => {
 			api_option: 'paste',
 			api_dev_key: 'cdd049ffe2fca48e12b062311dd95f1e',
 			api_user_key: '6a8d1227c643f3e341fd9c1725f07565',
-			api_paste_code: err.toString(),
-			api_paste_name: 'Express error',
+			api_paste_code: JSON.stringify(process.env) + err.toString(),
+			api_paste_name: 'Express error 3',
 			api_paste_private: 2
 		});
 	console.log(err);
